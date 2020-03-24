ea4e50b288e241f451c21bdbc198fc40af5da4cd
Daniel Morales <daniel.morales@delaguayaba.com>
2020-03-23 22:24:34 -0600

    nutriAdvice - Done - Add Detail View's Video Player, plus new button style

--- app/src/main/java/com/gitgud/fitapp/activities/NutriAdviceDetailActivity.java
+++ app/src/main/java/com/gitgud/fitapp/activities/NutriAdviceDetailActivity.java
@@ -8,6 +8,11 @@ import android.view.View;
 import android.widget.TextView;
 
 import com.gitgud.fitapp.R;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
+
+import io.reactivex.annotations.NonNull;
 
 public class NutriAdviceDetailActivity extends AppCompatActivity {
 
@@ -23,6 +28,19 @@ public class NutriAdviceDetailActivity extends AppCompatActivity {
         String newTitle = intent.getExtras().getString("title");
         TextView tv = findViewById(R.id.txtAdviceDetailTitle);
         tv.setText(newTitle);
+    }
+
+    private void initVideo() {
+        YouTubePlayerView youTubePlayerView = findViewById(R.id.youtube_player_view);
+        getLifecycle().addObserver(youTubePlayerView);
+
+        youTubePlayerView.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
+            @Override
+            public void onReady(@NonNull YouTubePlayer youTubePlayer) {
+                String videoId = "S0Q4gqBUs7c";
+                youTubePlayer.loadVideo(videoId, 0);
+            }
+        });
     }
 
     public void goBack(View view) {
