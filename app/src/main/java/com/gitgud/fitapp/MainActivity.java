911c227635eefd40bef910bfec154621b25ab482
Adrian <adrian@firstfactory.com>
2020-03-24 23:30:12 -0600

    app bottom nav

--- app/src/main/java/com/gitgud/fitapp/MainActivity.java
+++ app/src/main/java/com/gitgud/fitapp/MainActivity.java
@@ -25,13 +25,13 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
+                case R.id.navigation_dashboard:
                     mTextMessage.setText(R.string.title_home);
                     return true;
-                case R.id.navigation_dashboard:
+                case R.id.navigation_fitness:
                     mTextMessage.setText(R.string.title_dashboard);
                     return true;
-                case R.id.navigation_notifications:
+                case R.id.navigation_nutrition:
                     mTextMessage.setText(R.string.title_notifications);
                     return true;
             }
--- app/src/main/java/com/gitgud/fitapp/activities/AuthorizedActivity.java
+++ app/src/main/java/com/gitgud/fitapp/activities/AuthorizedActivity.java
@@ -2,42 +2,61 @@ package com.gitgud.fitapp.activities;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.gitgud.fitapp.R;
+import com.gitgud.fitapp.fragments.DashboardFragment;
+import com.gitgud.fitapp.fragments.ExercisesFragment;
+import com.gitgud.fitapp.fragments.NutritionalFragment;
+import com.gitgud.fitapp.fragments.RewardsFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class DashboardActivity extends AppCompatActivity {
+public class AuthorizedActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
-        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.navigation_dashboard:
-                        Toast.makeText(DashboardActivity.this, "Recents", Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.navigation_fitness:
-                        Toast.makeText(DashboardActivity.this, "Favorites", Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.navigation_nutrition:
-                        Toast.makeText(DashboardActivity.this, "Nearby", Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.navigation_rewards:
-                        Toast.makeText(DashboardActivity.this, "Nearby", Toast.LENGTH_SHORT).show();
-                        break;
+        setContentView(R.layout.activity_authorized);
+        //loading the default fragment
+        loadFragment(new DashboardFragment());
+        //getting bottom navigation view and attaching the listener
+        BottomNavigationView navigation = findViewById(R.id.bottom_navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
+    }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+        switch (item.getItemId()) {
+            case R.id.navigation_dashboard:
+                fragment = new DashboardFragment();
+                break;
+            case R.id.navigation_fitness:
+                fragment = new ExercisesFragment();
+                break;
+            case R.id.navigation_nutrition:
+                fragment = new NutritionalFragment();
+                break;
+            case R.id.navigation_rewards:
+                fragment = new RewardsFragment();
+                break;
+        }
+        return loadFragment(fragment);
+    }
 
-                }
-                return true;
-            }
-        });
+    private boolean loadFragment(Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
     }
 }
