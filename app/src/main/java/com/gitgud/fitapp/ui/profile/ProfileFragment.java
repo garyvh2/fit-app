0bd1152423eedf7a1468620ca5e3ecab1d68b5d5
Adrian <adrian@firstfactory.com>
2020-04-06 22:17:26 -0600

    profile UI init

--- app/src/main/java/com/gitgud/fitapp/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/gitgud/fitapp/ui/profile/ProfileFragment.java
@@ -1,6 +1,7 @@
 package com.gitgud.fitapp.ui.profile;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -9,8 +10,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.gitgud.fitapp.R;
+import com.github.lzyzsd.circleprogress.ArcProgress;
 
 public class ProfileFragment extends Fragment {
 
@@ -18,14 +22,57 @@ public class ProfileFragment extends Fragment {
     public ProfileFragment() {
         // Required empty public constructor
     }
+    Resources.Theme theme;
 
+    TextView lblBMI;
+    TextView lblBMIResult;
+
+    ArcProgress progressBar;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View fragmentView = inflater.inflate(R.layout.fragment_profile, container, false);
+        theme = fragmentView.getContext().getTheme();
+        progressBar = fragmentView.findViewById(R.id.imc_progress);
+        this.setIMCColors();
+        return fragmentView;
+    }
+
+    public  void  setIMCColors() {
+        Float imc = Float.parseFloat(progressBar.getProgress()+progressBar.getSuffixText().trim());
+
+
+        progressBar.setFinishedStrokeColor(getBMIColor(imc));
+        progressBar.setTextColor(getBMIColor(imc));
+    }
+
+
+    private int getBMIColor(double bmi) {
+        if (bmi > 40) return getResources().getColor(R.color.red, theme);
+        if (bmi > 35) return getResources().getColor(R.color.red, theme);
+        if (bmi > 30) return getResources().getColor(R.color.orange, theme);
+        if (bmi > 25) return getResources().getColor(R.color.colorAccent, theme);
+        if (bmi > 18.5) return getResources().getColor(R.color.aquaGreen, theme);
+        if (bmi > 17) return getResources().getColor(R.color.colorAccent, theme);
+        return getResources().getColor(R.color.orange, theme);
+    }
+
+    private int getBMIProgress(double bmi) {
+        if (bmi > 40) bmi = 40;
+        return (int) (75 * (bmi/40));
+    }
+
+    private String getBMIResult(double bmi) {
+        if (bmi > 40) return getResources().getString(R.string.obese3);
+        if (bmi > 35) return getResources().getString(R.string.obese2);
+        if (bmi > 30) return getResources().getString(R.string.obese1);
+        if (bmi > 25) return getResources().getString(R.string.overweight);
+        if (bmi > 18.5) return getResources().getString(R.string.normal_weight);
+        if (bmi > 17) return getResources().getString(R.string.underweight);
+        return getResources().getString(R.string.super_underweight);
     }
 
 
