a08f697d79076da454940b27a4d54dbccf30534f
Adrian <adrian@firstfactory.com>
2020-04-22 16:28:27 -0600

    added mising nav

--- app/src/main/java/com/gitgud/fitapp/ui/exercises/ExercisesFragment.java
+++ app/src/main/java/com/gitgud/fitapp/ui/exercises/ExercisesFragment.java
@@ -29,7 +29,7 @@ public class ExercisesFragment extends Fragment {
     ArrayList<MenuCardItem> menuItems = new ArrayList<>();
 
     public  ExercisesFragment(){
-        menuItems.add(new MenuCardItem(R.layout.menu_card_component ,R.drawable.ic_walk, "Steps Counter", StepsActivity.class));
+        menuItems.add(new MenuCardItem(R.layout.menu_card_component ,R.drawable.ic_walk, "Steps Counter", R.id.stepsFragment));
         menuItems.add(new MenuCardItem(R.layout.menu_card_component ,R.drawable.ic_fitness_center_black_24dp, "Routines", R.id.routinesFragment));
     }
 
